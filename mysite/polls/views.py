1128532aa59a0eda7cf1f02b021994e79310692d
Adam Benabou <adam.benabou@student.isae-supaero.fr>
2024-04-11 09:09:40 +0200

    Mise en place des vues génériques

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from .models import Choice , Question
-from django.shortcuts import get_object_or_404
-from django.shortcuts import HttpResponse
-from django.http import HttpResponseRedirect, HttpResponse
-from django.template import loader
+from django.views import generic
 
+from .models import Choice, Question
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
