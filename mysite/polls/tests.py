a261fcaf7c85225c62ad4675e1eb2d3594ce22c2
Adam Benabou <adam.benabou@student.isae-supaero.fr>
2024-04-11 12:26:00 +0200

    Modification et developpement de l'interface administrateur du site

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -47,7 +47,7 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
-def create_question(question_text, days):
+
     """
     Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
